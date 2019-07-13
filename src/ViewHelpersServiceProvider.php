6694c468cf88b645309141f5f2bfcdbfaeaf6373
Dean Blackborough <dean@g3d-development.com>
2019-07-13 16:51:57 +0100

    Test view helper
    - Correst service provider
    - Test view helper

--- src/ViewHelpersServiceProvider.php
+++ src/ViewHelpersServiceProvider.php
@@ -2,9 +2,21 @@
 
 namespace DBlackborough\ViewHelpers;
 
-use Illuminate\\Support\ServiceProvider;
+use Illuminate\Support\ServiceProvider;
 
 class ViewHelpersServiceProvider extends ServiceProvider
 {
+    public function boot()
+    {
+        $this->loadViewsFrom(__DIR__ . '/resources/views','laravel-view-helpers');
 
+        $this->publishes([
+            __DIR__ . '/resources/views' => resource_path('views/vendor/laravel-view-helpers')
+        ]);
+    }
+
+    public function register()
+    {
+        //
+    }
 }
