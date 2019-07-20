49ab6011ef03446b10dc8dd1936eea18e4226faa
Dean Blackborough <dean@g3d-development.com>
2019-07-20 15:14:52 +0100

    Added title tags
    - Added missing title tags.
    - Added CHANGELOG.

---
+++ CHANGELOG.md
@@ -0,0 +1,19 @@
+# Changelog
+
+The complete changelog, follows the format defined at https://keepachangelog.com/en/1.0.0/
+
+## [v1.00.2] - 2019-07-20
+### Added
+- Add CHANGELOG.
+
+### Changed
+- Added relevant title attributes.
+
+## [v1.00.1] - 2019-07-13
+### Fixed
+- Exmaple incorrect.
+- Var missing dollar `$` in view.
+
+## [v1.00.0] - 2019-07-13
+### Added
+- Initial release, pagination view helper for Laravel.
--- src/resources/views/pagination.blade.php
+++ src/resources/views/pagination.blade.php
@@ -8,9 +8,9 @@
     <div class="row">
         <div class="pages{{ $css_classes['left'] }}">
             @if ($offset > 0)
-            <a href="{{ $uri['base'] }}?offset={{ $offset - $limit }}&limit={{ $limit }}{{ $parameters }}{{ $uri['anchor'] }}" class="btn btn-primary"><</a>
+            <a href="{{ $uri['base'] }}?offset={{ $offset - $limit }}&limit={{ $limit }}{{ $parameters }}{{ $uri['anchor'] }}" title="Previous" class="btn btn-primary"><</a>
             @else
-            <a class="btn btn-primary disabled"><</a>
+            <a class="btn btn-primary disabled" title="Previous (disabled)"><</a>
             @endif
             <a class="btn btn-primary active">
                 @if ($count_prefix !== null) <span class="d-none d-sm-inline">{{ $count_prefix }}</span> @endif
@@ -24,16 +24,16 @@
                 {{ $total }}
             </a>
             @if ($total > ($offset + $limit))
-            <a href="{{ $uri['base'] }}?offset={{ $offset + $limit }}&limit={{ $limit }}{{ $parameters }}{{ $uri['anchor'] }}" class="btn btn-primary">></a>
+            <a href="{{ $uri['base'] }}?offset={{ $offset + $limit }}&limit={{ $limit }}{{ $parameters }}{{ $uri['anchor'] }}" title="next" class="btn btn-primary">></a>
             @else
-            <a class="btn btn-primary disabled">></a>
+            <a class="btn btn-primary disabled" title="Next (disabled)">></a>
             @endif
         </div>
         <div class="per-page{{ $css_classes['right'] }}">
             <form class="form-inline">
-                <select class="form-control form-control-sm per-page" data-uri="{{ $uri['base'] }}">
+                <select class="form-control form-control-sm per-page" data-uri="{{ $uri['base'] }}" title="Pagination limit">
                     @foreach ($limit_options as $option)
-                    <option value="{{ $option }}" @if ($option === $limit) selected="selected" @endif>{{ $option }}</option>
+                    <option value="{{ $option }}" @if ($option == $limit) selected="selected" @endif>{{ $option }}</option>
                     @endforeach
                 </select>
             </form>
